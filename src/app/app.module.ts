6220d32b94c59f188fa9ba7e06bc7bfe48cca427
Ty Mondragon <tymondragon@gmail.com>
2019-08-17 23:21:11 -0600

    add svg to template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SvgComponent } from './app/svg/svg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SvgComponent
   ],
   imports: [
     BrowserModule,
